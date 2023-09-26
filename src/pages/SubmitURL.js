f3c8ea3b9b6e5edc6eb137949a087efb933389b4
dolto <hdw810@daum.net>
2023-09-26 14:28:27 +0900

    dbsee 시작

--- src/pages/SubmitURL.js
+++ src/pages/SubmitURL.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import s from '../style'
+import {useNavigate } from 'react-router-dom';
 
 const SubmitURL = () => {
     let [url, set_url] = useState('');
+    const navigate = useNavigate();
     const fn_get_url = async (e) => {
         e.preventDefault();
         try {
@@ -12,10 +14,9 @@ const SubmitURL = () => {
             //console.log(rss);
             localStorage.setItem('data', rss);
             console.log(JSON.parse(localStorage.getItem('data')));
-
-            
+            navigate("/jsonReader")
         }catch{
-
+            alert("Json데이터가 아니거나 링크가 잘못되었습니다!")
         }
     }
 
